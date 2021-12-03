f4f622c3004f576616a489c8d8bf03908453e25b
Cory <oneillco117@gmail.com>
2021-12-02 20:21:53 -0500

    commit tree 1018eb29fac85449e983570eff2fc1786c1c7036

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,29 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    cheese = Cheese.find_by(params[:id])
+  render json: cheese
+  end
+
 end
+
+# def index 
+#   cheeses = Cheese.all
+#   render json: cheese
+# end
+
+# def show 
+#   cheese = Cheese.find_by(params[:id])
+#   render json: cheese 
+# end
+
+# def index 
+#   cheeses = Cheese.all 
+#   render json: cheese 
+# end
+
+# def show 
+#   cheese = Cheese.find_by(params[:id])
+#   render json: cheese
+# end
